fbf249e63dc1621090e379fff37d9b8a9a9f12d3
senierr <senierr_zhou@163.com>
2018-05-02 23:07:16 +0800

    更新注册登录

--- app/src/main/java/com/hideactive/entrance/SplashActivity.kt
+++ app/src/main/java/com/hideactive/entrance/SplashActivity.kt
@@ -1,15 +1,13 @@
 package com.hideactive.entrance
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import com.hideactive.R
 import com.hideactive.base.BaseActivity
 import com.hideactive.extension.bindToLifecycle
 import com.hideactive.extension.observeOnUI
 import com.hideactive.extension.subscribeOnIO
-import com.senierr.repository.Repository
-import com.senierr.repository.service.api.IUserService
-import com.senierr.repository.util.LogUtil
+import com.hideactive.user.RegisterActivity
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
@@ -19,24 +17,11 @@ class SplashActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-
-
         Observable.timer(2, TimeUnit.SECONDS)
                 .subscribeOnIO()
                 .observeOnUI()
                 .subscribe {
-//                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-
-                    val userService = Repository.getService<IUserService>()
-                    userService.login("zhou", "123456")
-                            .subscribeOnIO()
-                            .observeOnUI()
-                            .subscribe({
-                                LogUtil.logE(it.toString())
-                            }, {
-                                LogUtil.logE(Log.getStackTraceString(it))
-                            })
-
+                    startActivity(Intent(this@SplashActivity, RegisterActivity::class.java))
                 }
                 .bindToLifecycle(this)
     }
